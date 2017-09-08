8c208f1faf18abbc5201bdbd6069376f4fddcb36
Pravesh Choudhary <choudharypravesh03@gmail.com>
2017-09-08 13:44:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function (req, res) {
+    res.sendFile('./index.html', {root: __dirname })
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
