e38da145d7f874daf19bb9ce41c0313800b33646
Pravesh Choudhary <choudharypravesh03@gmail.com>
2017-09-08 14:24:01 +0530

    port changed according to hiroku

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
     res.sendFile('./index.html', {root: __dirname })
 })
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Example app listening on port '+process.env.PORT || 5000)
 })
